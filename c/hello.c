ee98684d6c349010a5f87d37664d8194b75fb282
jvcByte <jvc8463@gmail.com>
2026-05-11 16:42:12 +0100

    feat: add cs50 lib and Makefile

--- c/hello.c
+++ c/hello.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <cs50.h>
 
-int main(void){
-    printf(
-        "Hello world!\n"
-
-    );
+int main(void)
+{
+    string answer = get_string("What is your name?  ");
+    printf("Hello, %s\n", answer);
+    return 0;
 }
