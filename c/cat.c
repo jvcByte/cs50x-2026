c00b56a14f3a8ae544c78677e9780795c2756adf
jvcByte <jvc8463@gmail.com>
2026-05-13 17:06:37 +0100

    add cat.c

--- c/cat.c
+++ c/cat.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <cs50.h>
+
+void meow(int n);
 
 int main(void){
+    int n = get_int("Meow times? ");
+    meow(n);
+    
+}
 
-    for(int i = 0; i < 3; i++){
+void meow(int meow_times){
+
+    if (meow_times < 0) {
+        printf("No negative int allowed!");
+    }
+
+    for(int i = 0; i < meow_times; i++){
         printf("Meow!\n");
     }
 }
