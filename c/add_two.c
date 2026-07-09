077395b4f17f64c7ee1a2adcdd61515ca701c3f8
jvcByte <jvc8463@gmail.com>
2026-07-09 22:12:02 +0100

    update .gitignore, update c/add_two.c

--- c/add_two.c
+++ c/add_two.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 void add_two(int arr[], int len, int num) {
-    int first = arr[0];
 
-    for (int i = 0; i < len; i++) {
-        if (arr[i] == num - first) {
-            printf("indices: %d %d", 0, i); 
+    int found = 0;
+
+    for (int i = 0; i < len && !found; i++) {
+
+        for (int j = 0; j < len; j++){
+            if (arr[j] + arr[i] == num) {
+                printf("Found: i: %d j: %d arr[i]: %d arr[j]: %d\n", i, j, arr[i], arr[j]);
+                printf("Target: %d \nSum: %d and %d \nFound at Indices: %d and %d\n", num, arr[i], arr[j], i, j);
+                found = 1;
+                break;
+            } else {
+                printf("Not Found: i: %d j: %d arr[i]: %d arr[j]: %d\n", i, j, arr[i], arr[j]);
+            }
         }
     }
 }
@@ -16,5 +25,6 @@ int main() {
     int arr_len = sizeof(arr) / sizeof(arr[0]);
 
     add_two(arr, arr_len, 12);
+
     return 0;
 }
